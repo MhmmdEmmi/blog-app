e6d27e283b617813ee62f3fff2b6ead82917ea2e
Mohammad Emami <mhmmd.emmi@gmail.com>
2024-10-19 22:21:38 +0330

    Implementation of whether you are logged in and implementation of the logout function

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,14 +1,45 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUsername(userInfo.username);
+      });
+    });
+  }, []);
+
+  function logout() {
+    fetch("http://localhost:4000/logout", {
+      credentials: "include",
+      method: "POST",
+    });
+    setUsername(null);
+  }
+
   return (
     <header className="flex justify-between items-center mt-5 mb-12">
       <Link to="/" className="font-bold">
         کافه بلاگ
       </Link>
       <nav className="flex gap-4">
-        <Link to="/register">ثبت نام</Link>
-        <Link to="/login">ورود</Link>
+        {username && (
+          <>
+            <Link to="/create">ایجاد پست جدید</Link>
+            <a onClick={logout}>خروج</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/register">ثبت نام</Link>
+            <Link to="/login">ورود</Link>
+          </>
+        )}
       </nav>
     </header>
   );
