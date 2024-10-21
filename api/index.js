025d07d19c20cada643756e506482f347a3ac70a
Mohammad Emami <mhmmd.emmi@gmail.com>
2024-10-21 19:57:44 +0330

    Fetching all posts from the database and displaying them on the main page

--- api/index.js
+++ api/index.js
@@ -21,6 +21,7 @@ const secret = "eihpq8934yru2hfdg2983gfonoaweu98sdrgn";
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 mongoose.connect("mongodb://127.0.0.1:27017/users");
 
@@ -77,15 +78,29 @@ app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
   const newPath = path + "." + ext;
   fs.renameSync(path, newPath);
 
-  const { title, summary, content } = req.body;
-  const postDoc = await Post.create({
-    title,
-    summary,
-    content,
-    cover: newPath,
-  });
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { title, summary, content } = req.body;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover: newPath,
+      author: info.id,
+    });
 
-  res.json(postDoc);
+    res.json(postDoc);
+  });
+});
+
+app.get("/post", async (req, res) => {
+  res.json(
+    await Post.find()
+      .populate("author", ["username"])
+      .sort({ createdAt: -1 })
+      .limit(20)
+  );
 });
 
 app.listen(4000);
--- client/src/Post.js
+++ client/src/Post.js
@@ -1,30 +1,28 @@
-export default function Post() {
+export default function Post({
+  title,
+  summary,
+  cover,
+  content,
+  createdAt,
+  author,
+}) {
   return (
     <div className="grid grid-cols-3 gap-5 mb-7">
       {/* Image */}
       <div className="col-span-1">
-        <img
-          src="https://images.squarespace-cdn.com/content/v1/60a50f4518844e32d8c33c5f/1715063379900-7DOUUDQMGMBIB4B4G32T/IMG_0559.jpg?format=2500w"
-          alt=""
-        />
+        <img src={"http://localhost:4000/" + cover} alt="" />
       </div>
       {/* Texts */}
       <div className="col-span-2">
-        <h2 className="text-xl font-bold">فهرستی از زیباترین کافه های جهان</h2>
+        <h2 className="text-xl font-bold">{title}</h2>
         {/* Information paragraph */}
         <p className="flex gap-2 my-3 text-zinc-500 text-xs font-bold">
           {/* Author */}
-          <a className="text-zinc-800">محمد امامی</a>
-          <time>1403/7/26 08:17:38PM</time>
+          <a className="text-zinc-800">{author.username}</a>
+          <time>{createdAt}</time>
         </p>
         {/* Summary of the post */}
-        <p className="leading-5">
-          اندرو چانگ معمار، عکاس، طراح وب سایت، نوشیدنی قهوه و بنیانگذار Best
-          Cafe Designs است. سایت او رشد فرهنگ جهانی کافه ها را ترسیم می کند و با
-          بیش از 40 معمار، عکاس و نویسنده همکاری می کند. در این مصاحبه متوجه
-          می‌شویم که ایده بهترین طرح‌های کافه چگونه شکل گرفت، کافه‌هایی که او
-          ارائه می‌کند، گرایش‌های طراحی، طراحی وب‌سایت و برترین کافه‌های او.
-        </p>
+        <p className="leading-5">{summary}</p>
       </div>
     </div>
   );
